bd3f3320ee5f3ed483be3f0661ece68ecda6f58c
杨帅 <57125827@qq.com>
2022-10-15 18:08:12 +0800

    构建Android项目

---
+++ app/src/main/java/com/yangshuai/ystiktok/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yangshuai.ystiktok
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
